21e996fc80794d77f573f869d761d873c1d5dd1b
masnagam <masnagam@gmail.com>
2021-05-24 17:32:10 +0900

    fix(scripts/debian.apt): fix typo

--- scripts/debian.apt.sh
+++ scripts/debian.apt.sh
@@ -7,8 +7,13 @@ sudo sed -i 's/^\(deb .*\) main$/\1 main contrib non-free/' /etc/apt/sources.lis
 sudo sed -i 's/^\(deb-src .*\) main$/\1 main contrib non-free/' /etc/apt/sources.list
 
 # use backports
-cat <<'EOF' > sudo tee /etc/apt/sources.list.d/backports.list
+cat <<'EOF' | sudo tee /etc/apt/sources.list.d/backports.list >/dev/null
 deb http://deb.debian.org/debian buster-backports main contrib non-free
 EOF
 
 sudo apt-get update
+
+# tests
+grep 'main contrib non-free' /etc/apt/sources.list >/dev/null
+test -f /etc/apt/sources.list.d/backports.list
+grep 'buster-backports' /etc/apt/sources.list.d/backports.list >/dev/null
