e06e91429aae9d5ecbd7c910498d8c45cab6f834
masnagam <masnagam@gmail.com>
2021-05-24 18:07:42 +0900

    refactor(scripts/debian.apt): simplify sources.list

--- scripts/debian.apt.sh
+++ scripts/debian.apt.sh
@@ -1,19 +1,17 @@
 echo "Configuring APT..."
 
+VERSION=buster
+# add contrib and non-free, use backports
 # TODO: Should use https instead of http?
-
-# add contrib and non-free
-sudo sed -i 's/^\(deb .*\) main$/\1 main contrib non-free/' /etc/apt/sources.list
-sudo sed -i 's/^\(deb-src .*\) main$/\1 main contrib non-free/' /etc/apt/sources.list
-
-# use backports
-cat <<'EOF' | sudo tee /etc/apt/sources.list.d/backports.list >/dev/null
-deb http://deb.debian.org/debian buster-backports main contrib non-free
+cat <<EOF | sudo tee /etc/apt/sources.list >/dev/null
+deb http://deb.debian.org/debian $VERSION main contrib non-free
+deb-src http://deb.debian.org/debian $VERSION main contrib non-free
+deb http://security.debian.org/debian-security $VERSION/updates main contrib non-free
+deb-src http://security.debian.org/debian-security $VERSION/updates main contrib non-free
+deb http://deb.debian.org/debian $VERSION-updates main contrib non-free
+deb-src http://deb.debian.org/debian $VERSION-updates main contrib non-free
+deb http://deb.debian.org/debian $VERSION-backports main contrib non-free
+deb-src http://deb.debian.org/debian $VERSION-backports main contrib non-free
 EOF
 
 sudo apt-get update
-
-# tests
-grep 'main contrib non-free' /etc/apt/sources.list >/dev/null
-test -f /etc/apt/sources.list.d/backports.list
-grep 'buster-backports' /etc/apt/sources.list.d/backports.list >/dev/null
