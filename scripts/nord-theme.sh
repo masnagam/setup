51108708849e5ac03dd340bc86b88495298fee7c
masnagam <masnagam@gmail.com>
2021-05-31 18:02:42 +0900

    fix(scripts/nord-theme): wrong url

--- scripts/nord-theme.sh
+++ scripts/nord-theme.sh
@@ -1,8 +1,11 @@
+BASEURL=https://raw.githubusercontent.com/arcticicestudio
 echo "Installing .dir_colors..."
-curl -fsSL https://github.com/arcticicestudio/nord-dircolors/blob/develop/src/dir_colors \
-  >$HOME/.dir_colors
+curl -fsSL $BASEURL/nord-dircolors/develop/src/dir_colors >$HOME/.dir_colors
 
 echo "Installing .Xresources.d/color-theme..."
 mkdir -p $HOME/.Xresources.d
-curl -fsSL https://raw.githubusercontent.com/arcticicestudio/nord-xresources/develop/src/nord \
-  >$HOME/.Xresources.d/color-theme
+curl -fsSL $BASEURL/nord-xresources/develop/src/nord >$HOME/.Xresources.d/color-theme
+
+# tests
+cat $HOME/.dir_colors | head -1 | grep 'Arctic Ice Studio' >/dev/null
+cat $HOME/.Xresources.d/color-theme | head -1 | grep 'Arctic Ice Studio' >/dev/null
