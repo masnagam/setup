d9ac8ccc137f54e090d770ba73f682859cb7de81
masnagam <masnagam@gmail.com>
2021-06-07 22:29:21 +0900

    feat(scripts/linux.fcitx-mozc): configure fcitx

--- scripts/linux.fcitx-mozc.sh
+++ scripts/linux.fcitx-mozc.sh
@@ -3,7 +3,8 @@ echo "Installing fcitx-mozc..."
 case $SETUP_TARGET in
   debian)
     sudo apt-get install -y --no-install-recommends \
-      dbus-x11 fcitx fcitx-config-gtk fcitx-mozc mozc-utils-gui
+      dbus-x11 fcitx fcitx-config-gtk fcitx-frontend-gtk3 fcitx-mozc mozc-utils-gui \
+      fcitx-ui-light iso-codes
     ;;
   *)
     echo "ERROR: Target not supported: $SETUP_TARGET"
@@ -11,13 +12,50 @@ case $SETUP_TARGET in
     ;;
 esac
 
-# TODO: change settings in $HOME/.config/fcitx/config (or $XDG_CONFIG_HOME/fcitx/config).
+mkdir -p $HOME/.config/fcitx/addon
+mkdir -p $HOME/.config/fcitx/conf
+
+cat <<'EOF' >$HOME/.config/fcitx/config
+[Hotkey]
+TriggerKey=CTRL_\
+EOF
+
+cat <<'EOF' >$HOME/.config/fcitx/profile
+[Profile]
+IMName=mozc
+EnabledIMList=fcitx-keyboard-us:True,mozc:True
+EOF
+
+cat <<'EOF' >$HOME/.config/fcitx/addon/fcitx-light-ui.config
+[Addon]
+Enabled=True
+EOF
+
+cat <<'EOF' >$HOME/.config/fcitx/conf/fcitx-xim.config
+[Xim]
+UseOnTheSpotStyle=True
+EOF
+
+cat <<'EOF' >$HOME/.config/fcitx/conf/fcitx-light-ui.config
+[LightUI]
+Font=Sarasa Term J
+FontLocale=en_US.UTF-8
+UseTray=False
+MainWindowHideMode=Hide
+EOF
 
 mkdir -p $HOME/.xinitrc.d
 cat <<'EOF' >$HOME/.xinitrc.d/ime.sh
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS='@im=fcitx'
+EOF
+
+mkdir -p $HOME/.Xresources.d
+cat <<'EOF' >$HOME/.Xresources.d/input-method
+URxvt.perl-ext-common: xim-onthespot
+URxvt.inputMethod: fcitx
+URxvt.preeditType: OnTheSpot
 EOF
 
 # tests
