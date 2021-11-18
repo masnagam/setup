0ada1401ea5bc44acc0903feb332219e56b057fa
masnagam <masnagam@gmail.com>
2021-11-18 23:10:50 +0900

    feat(debian): docker compose v2

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,14 @@
 echo "Installing docker..."
 
+if [ -n "$SETUP_GITHUB_TOKEN" ]
+then
+  GITHUB_API_AUTH_HEADER="-H Authorization: token $SETUP_GITHUB_TOKEN"
+else
+  GITHUB_API_AUTH_HEADER=
+fi
+
+DOCKER_COMPOSE_LATEST_URL=https://api.github.com/repos/docker/compose/releases/latest
+
 case $SETUP_TARGET in
   arch)
     if ! which paru >/dev/null 2>&1
@@ -17,16 +26,27 @@ case $SETUP_TARGET in
         echo "WARN: get.docker.com doesn't support $arch, install an older version by using apt"
         sudo apt-get install -y --no-install-recommends docker.io docker-compose
         ;;
-      x86_64 | armv7l | aarch64)
+      x86_64 | armv7* | aarch64)
+        sudo apt-get install -y --no-install-recommends jq
         if ! which docker >/dev/null
         then
           curl -sSL https://get.docker.com | sudo sh
         fi
-        curl -fsSL https://raw.githubusercontent.com/masnagam/sbc-scripts/main/get-docker-compose | \
-          sh | sudo tar -x -C /usr/local/bin
+        ARCH=$(arch)
+        if [ "$ARCH" = armv7l ]
+        then
+          ARCH=armv7
+        fi
+        DL_URL="$(curl $GITHUB_API_AUTH_HEADER -fsSL $DOCKER_COMPOSE_LATEST_URL | jq -Mr '.assets[].browser_download_url' | grep -e docker-compose-linux-$ARCH\$)"
+        # Install to /usr/local/lib/docker/cli-plugins so that it works with sudo.
+        sudo mkdir -p /usr/local/lib/docker/cli-plugins
+        sudo curl $DL_URL -fsSL -o /usr/local/lib/docker/cli-plugins/docker-compose
+        sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
         ;;
     esac
     sudo usermod -aG docker $(whoami) || true
+    sudo systemctl start docker
+    sudo systemctl enable docker
     ;;
   macos)
     if ! which -s brew
@@ -45,5 +65,12 @@ esac
 if [ "$SETUP_TARGET" != macos ]
 then
   sudo docker version
-  sudo docker-compose version
+  case $(arch) in
+    i386 | i686)
+      sudo docker-compose version
+      ;;
+    *)
+      sudo docker compose version
+      ;;
+  esac
 fi
