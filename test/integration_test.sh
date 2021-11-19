8c7d3dfb49525e5d7e138937a9e883b042b5dcfb
masnagam <masnagam@gmail.com>
2021-11-19 18:12:55 +0900

    test: pass SETUP_GITHUB_TOKEN to sh running on VM

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -3,16 +3,22 @@ set -e
 TARGET="$1"
 shift
 
+ENVS=$(cat <<EOF | tr '\n' ' '
+SETUP_DEBUG=1
+SETUP_GITHUB_TOKEN=$SETUP_GITHUB_TOKEN
+EOF
+)
+
 vagrant up $TARGET
 
 # Run twice in order to check idempotence of the script.
 echo "============================== 1st run =============================="
-vagrant ssh $TARGET -- env SETUP_DEBUG=1 sh /vagrant/$TARGET.sh "$@"
+vagrant ssh $TARGET -- env $ENVS sh /vagrant/$TARGET.sh "$@"
 echo "============================== 2nd run =============================="
-vagrant ssh $TARGET -- env SETUP_DEBUG=1 sh /vagrant/$TARGET.sh "$@"
+vagrant ssh $TARGET -- env $ENVS sh /vagrant/$TARGET.sh "$@"
 
-vagrant ssh $TARGET -c 'env SETUP_DEBUG=1 run-setup-script bash'
-vagrant ssh $TARGET -c 'env SETUP_DEBUG=1 fetch-setup-file emacs.init.el | head -1'
+vagrant ssh $TARGET -c "env $ENVS run-setup-script bash"
+vagrant ssh $TARGET -c "env $ENVS fetch-setup-file emacs.init.el | head -1"
 
 # The VM won't be destroyed when any test fails so that it makes it possible to debug the failure.
 vagrant destroy -f $TARGET
