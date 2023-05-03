df6bb4c86522f03a5ea8e574498e8eab1b2c62db
masnagam <masnagam@gmail.com>
2023-05-03 14:24:47 +0900

    fix(scripts/nord-theme): update urls

--- scripts/nord-theme.sh
+++ scripts/nord-theme.sh
@@ -1,18 +1,18 @@
-BASEURL=https://raw.githubusercontent.com/arcticicestudio
+BASEURL=https://raw.githubusercontent.com/nordtheme
 
 echo "Installing .dir_colors..."
-curl -fsSL $BASEURL/nord-dircolors/develop/src/dir_colors >$HOME/.dir_colors
+curl -fsSL $BASEURL/dircolors/develop/src/dir_colors >$HOME/.dir_colors
 
 mkdir -p $HOME/.profile.d
 cat <<'EOF' >$HOME/.profile.d/dircolors.sh
-# https://github.com/arcticicestudio/nord-dircolors#activation
+# https://github.com/nordtheme/dircolors#activation
 test -r ~/.dir_colors && eval $(dircolors ~/.dir_colors)
 EOF
 
 echo "Installing .Xresources.d/color-theme..."
 mkdir -p $HOME/.Xresources.d
-curl -fsSL $BASEURL/nord-xresources/develop/src/nord >$HOME/.Xresources.d/color-theme
+curl -fsSL $BASEURL/xresources/develop/src/nord >$HOME/.Xresources.d/color-theme
 
 # tests
-cat $HOME/.dir_colors | head -1 | grep 'Arctic Ice Studio' >/dev/null
-cat $HOME/.Xresources.d/color-theme | head -1 | grep 'Arctic Ice Studio' >/dev/null
+cat $HOME/.dir_colors | grep 'FILE 00' >/dev/null
+cat $HOME/.Xresources.d/color-theme | grep '#define nord0 #2E3440' >/dev/null
