1cabe94de5155cdaffb01777a5c996bdd8a175d4
masnagam <masnagam@gmail.com>
2023-06-27 20:13:53 +0900

    refactor(scripts/apt.debian.sh): refactoring

--- scripts/apt.debian.sh
+++ scripts/apt.debian.sh
@@ -12,10 +12,10 @@ VERSION=bookworm
 cat <<EOF | sudo tee /etc/apt/sources.list >/dev/null
 deb http://deb.debian.org/debian $VERSION main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian $VERSION main contrib non-free non-free-firmware
-deb http://security.debian.org/debian-security $VERSION-security main contrib non-free non-free-firmware
-deb-src http://security.debian.org/debian-security $VERSION-security main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian $VERSION-updates main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian $VERSION-updates main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security $VERSION-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security $VERSION-security main contrib non-free non-free-firmware
 EOF
 
 cat <<EOF | sudo tee /etc/apt/sources.list.d/backports.list >/dev/null
