10f0cbfe88216ec5b82c21e0933502a53a1a6cd6
masnagam <masnagam@gmail.com>
2025-05-17 08:37:54 +0900

    feat(linux): enable UPower service

--- scripts/bluetooth.linux.sh
+++ scripts/bluetooth.linux.sh
@@ -12,11 +12,15 @@ case $SETUP_TARGET in
     echo "Installing packages..."
     # use pipewire in arch
     paru -S --noconfirm bluez bluez-utils pipewire-pulse
+    # enable upower
+    paru -S --noconfirm upower
     ;;
   debian)
     echo "Installing packages..."
     # use puseaudio in debian
     sudo apt-get install -y --no-install-recommends bluez pulseaudio-module-bluetooth
+    # enable upower
+    sudo apt-get install -u --no-install-recommends upower
     ;;
   *)
     echo "ERROR: Target not supported: $SETUP_TARGET"
@@ -28,5 +32,11 @@ echo "Enabling bluetooth service..."
 sudo systemctl start bluetooth
 sudo systemctl enable bluetooth
 
+echo "Enabling UPower service..."
+sudo systemctl start upower
+sudo systemctl enable upower
+
 # tests
 bluetoothctl --version
+systemctl status bluetooth
+systemctl status upower
